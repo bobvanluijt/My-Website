dd5b967303bae9920ec3633b1cefd08efdf95354
bobvanluijt <bob@vanluijt.nl>
2014-09-21 21:18:14 +0200

    Adding about page

---
+++ about.php
@@ -0,0 +1,72 @@
+<div class="bigBlock" id="bigBlock__1">
+        <div class="bigBlockTitle">
+        	<div class="bigBlockTitleContent">About</div>
+        </div>
+        <div class="bigBlockContainer">
+            <div class="imageBlock">
+                <div class="content">
+                    <div class="infoImageCase" style="background-image:url(./attributes/bob.jpg)"></div>
+                    <span class="bigLetter">L</span><div class="contentText">orem ipsum dolor sit amet, consectetur adipiscing elit. Donec a faucibus elit. Quisque ac sapien odio. In hac habitasse platea dictumst. Maecenas at lobortis libero. Nunc venenatis neque ac orci tincidunt fringilla. Etiam erat mauris, rutrum et purus non, facilisis tincidunt magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere sollicitudin erat. Pellentesque dapibus fermentum lacus eu tincidunt.
+    
+    Praesent eleifend ipsum urna, id molestie est interdum nec. Integer ornare augue orci, non condimentum elit interdum ut. Vestibulum vel dolor vel eros tincidunt convallis posuere non sapien. Sed molestie luctus faucibus. Nunc a lacus lectus. Nam congue nisi sed tempus blandit. Ut non commodo massa.
+    
+    Proin vehicula eu quam in blandit. Vivamus malesuada imperdiet porta. Fusce lacinia lectus in augue lobortis, convallis ultrices arcu blandit. Aliquam non libero augue. In aliquet tincidunt cursus. Phasellus auctor dapibus ante, sed egestas urna vehicula nec. Sed fringilla, enim quis lobortis pretium, nunc lectus feugiat leo, vel dignissim sem tellus sed augue. Nulla scelerisque felis lorem, et fringilla diam consectetur et. Ut fringilla, libero ut ullamcorper consectetur, ex tortor cursus leo, nec rhoncus justo risus non ipsum.</div>
+                </div>
+            </div>
+        </div>
+        <div class="bigBlockTags">
+        	<div class="bigBlockTagsContent">
+        	    <div class="tag">Tags: <?=$_GET['i']?></div>
+        	</div>
+        </div>
+</div>
+
+
+<div class="bigBlock" id="bigBlock__2">
+        <div class="bigBlockTitle">
+        	<div class="bigBlockTitleContent">Contact</div>
+        </div>
+        <div class="bigBlockContainer">
+            <div class="imageBlock">
+                <div class="content">
+                    <div class="infoImageCase noShadow" style="background-image:url(./icons/twitter.png)"></div>
+                    <span class="bigLetter">Y</span><div class="contentText">ou can reach me through all the regular channels. Feel free to get in touch with me if you have any questions!
+                    <p>
+                    	<br />
+                        <table>
+                        	<tr>
+                            	<td style="width:1%">Twitter</td>
+                                <td><a href="https://twitter.com/bobvanluijt" target="_blank">twitter.com/bobvanluijt</a></td>
+                            </tr>
+                            <tr>
+                            	<td>Work</td>
+                                <td>bob[a]kubrickolo.gy</td>
+                            </tr>
+                            <tr>
+                            	<td>Personal</td>
+                                <td>bob[a]vanluijt.nl</td>
+                            </tr>
+                            <tr>
+                            	<td>Inquiries</td>
+                                <td>renate[a]kubrickolo.gy</td>
+                            </tr>
+                            <tr>
+                            	<td>Skype</td>
+                                <td><a href="skype:bobvanluijt?call" target="_blank">bobvanluijt</a></td>
+                            </tr>
+                            <tr>
+                            	<td>Phone</td>
+                                <td>+31 (0)6 109 35 031</td>
+                            </tr>
+                        </table> 
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bigBlockTags">
+        	<div class="bigBlockTagsContent">
+        	    <div class="tag">Tags: <?=$_GET['i']?></div>
+        	</div>
+        </div>
+</div>
