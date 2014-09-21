34aba10f1abb0af9acc8aab0b4eeb940a1a5e60d
bobvanluijt <bob@vanluijt.nl>
2014-09-21 21:16:03 +0200

    Lot's of updates, mostly CSS and JS

---
+++ art.php
@@ -0,0 +1,22 @@
+<div class="bigBlock" id="bigBlock__1">
+        <div class="bigBlockTitle">
+        	<div class="bigBlockTitleContent">Art</div>
+        </div>
+        <div class="bigBlockContainer">
+            <div class="imageBlock">
+                <div class="content">
+                    <div class="infoImageCase" style="background-image:url(./attributes/bob.jpg)"></div>
+                    <span class="bigLetter">L</span><div class="contentText">orem ipsum dolor sit amet, consectetur adipiscing elit. Donec a faucibus elit. Quisque ac sapien odio. In hac habitasse platea dictumst. Maecenas at lobortis libero. Nunc venenatis neque ac orci tincidunt fringilla. Etiam erat mauris, rutrum et purus non, facilisis tincidunt magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere sollicitudin erat. Pellentesque dapibus fermentum lacus eu tincidunt.
+    
+    Praesent eleifend ipsum urna, id molestie est interdum nec. Integer ornare augue orci, non condimentum elit interdum ut. Vestibulum vel dolor vel eros tincidunt convallis posuere non sapien. Sed molestie luctus faucibus. Nunc a lacus lectus. Nam congue nisi sed tempus blandit. Ut non commodo massa.
+    
+    Proin vehicula eu quam in blandit. Vivamus malesuada imperdiet porta. Fusce lacinia lectus in augue lobortis, convallis ultrices arcu blandit. Aliquam non libero augue. In aliquet tincidunt cursus. Phasellus auctor dapibus ante, sed egestas urna vehicula nec. Sed fringilla, enim quis lobortis pretium, nunc lectus feugiat leo, vel dignissim sem tellus sed augue. Nulla scelerisque felis lorem, et fringilla diam consectetur et. Ut fringilla, libero ut ullamcorper consectetur, ex tortor cursus leo, nec rhoncus justo risus non ipsum.</div>
+                </div>
+            </div>
+        </div>
+        <div class="bigBlockTags">
+        	<div class="bigBlockTagsContent">
+        	    <div class="tag">Tags: <?=$_GET['i']?></div>
+        	</div>
+        </div>
+</div>
